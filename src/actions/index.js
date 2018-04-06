49f9443e855aa01ed55f070b196a88e7655c859b
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-06 12:04:47 +0100

    fetch weather API call

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
 const API_KEY = '82e67981d0b8489837e69754e242a23f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+//samples
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},uk`;
+  return {
+    type: FETCH_WEATHER
+  };
+}
