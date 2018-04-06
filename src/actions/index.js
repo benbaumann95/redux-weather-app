7ac88b439685cf8daaa2b094beb758c9f07746b3
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-06 13:30:31 +0100

    sumbit ajax request for weather forecast

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = '82e67981d0b8489837e69754e242a23f';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 //samples
@@ -5,8 +7,11 @@ const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KE
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},uk`;
+  const url = `${ROOT_URL}&q=${city},gb`;
+  const request = axios.get(url);
+
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
