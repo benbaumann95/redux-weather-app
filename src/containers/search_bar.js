98ac9e8f4305c8dea563674fab93bf324d84202b
Ben Baumann <benbaumann95@hotmail.co.uk>
2018-04-06 11:39:05 +0100

    API key for weather forecast

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -13,10 +13,16 @@ class SearchBar extends React.Component {
     this.setState({ term: event.target.value })
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    //We need to go and fetch weather data
+  }
+
   render() {
     return (
       <div>
-        <form className='input-group'>
+        <form onSubmit={this.onFormSubmit} className='input-group'>
           <input
             placeholder='Get a 5-day forecast in your favourite cities'
             className='form-control'
